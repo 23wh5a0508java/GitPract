03be692f3aef6f45f5ef1ea8f8f979c0ab4d53ab
Shraddha_5353 <ranideshpande1986@gmail.com>
2024-12-13 05:02:06 +0530

    hello

---
+++ SumOfNumbers2.java
@@ -0,0 +1,13 @@
+public class SumOfNumbers2 {
+    public static void main(String args[]) {
+        int x = 10; // Hardcoded value
+        int y = 20; // Hardcoded value
+        int sum = sum(x, y);
+        System.out.println("The sum of two numbers x and y is: " + sum);
+    }
+
+    // Method that calculates the sum
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
